a5318b9e8e9fd84a75822efcaba315714aaf6f4f
Adriano Massi <adrianomassi@gmail.com>
2019-12-07 02:19:30 +0100

    Finished

--- lib/bloc/timer_bloc.dart
+++ lib/bloc/timer_bloc.dart
@@ -18,9 +18,62 @@ class TimerBloc extends Bloc<TimerEvent, TimerState> {
   TimerState get initialState => Ready(_duration);
 
   @override
+  void onTransition(Transition<TimerEvent, TimerState> transition) {
+    super.onTransition(transition);
+    print(transition);
+  }
+
+  @override
   Stream<TimerState> mapEventToState(
     TimerEvent event,
   ) async* {
-    // TODO: Add Logic
+    if (event is Start) {
+      yield* _mapStartToState(event);
+    } else if (event is Pause) {
+      yield* _mapPauseToState(event);
+    } else if (event is Resume) {
+      yield* _mapResumeToState(event);
+    } else if (event is Reset) {
+      yield* _mapResetToState(event);
+    } else if (event is Tick) {
+      yield* _mapTickToState(event);
+    }
+  }
+
+  @override
+  Future<void> close() {
+    _tickerSubscription?.cancel();
+    return super.close();
+  }
+
+  Stream<TimerState> _mapStartToState(Start start) async* {
+    yield Running(start.duration);
+    _tickerSubscription?.cancel();
+    _tickerSubscription = _ticker
+        .tick(ticks: start.duration)
+        .listen((duration) => add(Tick(duration: duration)));
+  }
+
+  Stream<TimerState> _mapPauseToState(Pause pause) async* {
+    if (state is Running) {
+      _tickerSubscription?.pause();
+      yield Paused(state.duration);
+    }
+  }
+
+  Stream<TimerState> _mapResumeToState(Resume pause) async* {
+    if (state is Paused) {
+      _tickerSubscription?.resume();
+      yield Running(state.duration);
+    }
+  }
+
+  Stream<TimerState> _mapResetToState(Reset reset) async* {
+    _tickerSubscription?.cancel();
+    yield Ready(_duration);
+  }
+
+  Stream<TimerState> _mapTickToState(Tick tick) async* {
+    yield tick.duration > 0 ? Running(tick.duration) : Finished();
   }
 }
--- lib/ticker.dart
+++ lib/ticker.dart
@@ -1,5 +1,6 @@
 class Ticker {
   Stream<int> tick({int ticks}) {
-    return Stream.periodic(Duration(seconds: 1), (x) => ticks - x - 1).take(ticks);
+    return Stream.periodic(Duration(seconds: 1), (x) => ticks - x - 1)
+        .take(ticks);
   }
 }
